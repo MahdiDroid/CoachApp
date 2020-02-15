3d52711bed517bad06e3a0efa41cd1cdc0704d5a
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-15 21:10:09 +0300

    beanlidecycleapp add

---
+++ src/com/company/BeanLifeCycleApp.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleApp {
+    public static void main(String[] args) {
+        //load configuration
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("BeanScope-applicationContext.xml");
+
+        ICoach theCoach = context.getBean("myCoach",ICoach.class);
+        ICoach alphaCoach = context.getBean("myCoach",ICoach.class);
+
+        boolean result = (theCoach == alphaCoach);
+        System.out.println("\n Pointing to the same object:   "+result);
+        System.out.println("Memory the coach " + theCoach );
+        System.out.println("Memory alpha  coach " +alphaCoach );
+
+    context.close();
+
+    }
+}
